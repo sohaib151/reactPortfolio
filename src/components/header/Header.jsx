360e9c72ef843ae83bcdaa1a92b893b6008b7b95
Your Name <you@example.com>
2022-12-16 23:15:44 +0000

    first co

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <header>
+    <header id='header'>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Sohaib Khan</h1>
